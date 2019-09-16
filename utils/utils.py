ca010563a7a6d2e0d8e7aa0cb0c6a0742b6d6f7b
Claudia Onorato <claudia.onorato@polymtl.ca>
2019-09-15 22:45:30 -0400

    added part 3 notebook

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,47 @@
 from pylsl import StreamInlet, resolve_byprop
 from muselsl.constants import LSL_SCAN_TIMEOUT, LSL_EEG_CHUNK
 from time import time
+from collections import OrderedDict
+from csv import reader
+
+
+def read_eeg_file(csv_file_path):
+    eeg_data = OrderedDict()
+
+    with open(csv_file_path, newline='') as eeg_file:
+        csv_reader = reader(eeg_file, delimiter=',')
+        eeg_data = OrderedDict({key:[] for key in next(csv_reader)})  # Use the headers as key of eeg_data dict
+        for samples in csv_reader:
+            for i, key in enumerate(eeg_data.keys()):
+                eeg_data[key].append(float(samples[i])) # Add the sample to its channel list
+    
+    return eeg_data
+
+
+def debounce_signal(signal, debouncing_time=0.1, period=1/256):
+    rising_edges = []
+    falling_edges = []
+    last_value = signal[0]
+
+    # Find the edge indexes.
+    for i, data in enumerate(signal):
+        if data is not last_value:
+            if last_value:
+                falling_edges.append(i)
+            else:
+                rising_edges.append(i)
+        last_value = data
+
+    for i in falling_edges:
+        next_rising_edge = next((index for index in rising_edges if index > i), None)
+        if next_rising_edge is None:
+            break
+        
+        # If the next rising edge is within the debouncing period, then set the blink signal to 1.
+        falling_to_rising_idx_span = next_rising_edge - i
+        if falling_to_rising_idx_span * period < debouncing_time:
+            signal[i: next_rising_edge] = [True] * falling_to_rising_idx_span
+    return signal
 
 
 def print_eeg_callback(timestamps, eeg_data):
